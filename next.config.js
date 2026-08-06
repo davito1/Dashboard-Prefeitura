ea77365d1d6f397cc1f458774eba665f5b8419cc
davito1 <89197789+davito1@users.noreply.github.com>
2026-08-06 19:47:58 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  experimental: {
-    serverActions: { bodySizeLimit: '25mb' }
-  }
-};
-
-module.exports = nextConfig;
